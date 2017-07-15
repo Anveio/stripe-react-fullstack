6b63f09451e225b5193d2f88e22c0c203e2289b9
Shovon Hasan <im.shovonhasan@gmail.com>
2017-07-15 00:03:36 -0400

    Fix routing and reformat routing in App.tsx

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -17,14 +17,11 @@ export interface Handlers {
   readonly onBoot: () => void;
 }
 
-export default class App extends React.Component<Props & Handlers, never> {
+export default class App extends React.PureComponent<Props & Handlers, never> {
   componentWillMount() {
-    if (localStorage.getItem('jwt')) {
-      this.props.onBoot();
-    } else {
-      // tslint:disable-next-line:no-console
-      console.log('No jwt in localstorage. Skipping automatic log in.');
-    }
+    localStorage.getItem('jwt')
+      ? this.props.onBoot() // tslint:disable-next-line:no-console
+      : console.log('No jwt in localstorage. Skipping automatic log in.');
   }
 
   render() {
@@ -34,24 +31,9 @@ export default class App extends React.Component<Props & Handlers, never> {
         <Page title="Dashboard">
           <Notifications />
           <Route exact path="/" component={Home} />
-          <Route
-            path="/signup"
-            render={() => {
-              return <SignupForm />;
-            }}
-          />
-          <Route
-            path="/login"
-            render={() => {
-              return <LoginForm />;
-            }}
-          />
-          <Route
-            path="/users"
-            render={() => {
-              return <UserList />;
-            }}
-          />
+          <Route path="/signup" component={() => <SignupForm />} />
+          <Route path="/login" component={() => <LoginForm />} />
+          <Route path="/users" component={() => <UserList />} />
         </Page>
       </main>
     );
