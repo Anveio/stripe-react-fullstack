092376fab28573bf39d9070cbd168233b523f283
Shovon Hasan <im.shovonhasan@gmail.com>
2017-07-22 14:42:03 -0400

    Use await instead of .then chains

--- server/controllers/stripeController.js
+++ server/controllers/stripeController.js
@@ -13,30 +13,22 @@ exports.processPayment = async (req, res) => {
   console.log(`Processing payment at: ${new Date().toISOString()}`);
   const { amount, currency, description, email } = req.body;
 
-  stripe.customers
-    .create({ email })
-    .then(customer => {
-      return stripe.customers.createSource(customer.id, {
-        source: {
-          object: 'card',
-          exp_month: 10,
-          exp_year: 2018,
-          number: '4242 4242 4242 4242',
-          cvc: 100
-        }
-      });
-    })
-    .then(source => {
-      return stripe.charges.create({
-        amount,
-        currency,
-        customer: source.customer
-      });
-    })
-    .then(charge => {
-      console.log(charge);
-    })
-    .catch(reason => console.error(reason));
+  const customer = await stripe.customers.create({ email });
+  const source = await stripe.customers.createSource(customer.id, {
+    source: {
+      object: 'card',
+      exp_month: 10,
+      exp_year: 2018,
+      number: '4242 4242 4242 4242',
+      cvc: 100
+    }
+  });
+
+  const charge = await stripe.charges.create({
+    amount,
+    currency,
+    customer: source.customer
+  });
 
   // const customer = await stripe.customers.create({email})
 
