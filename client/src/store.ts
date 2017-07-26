858cf877c4dd435fef6e35024465a16e00c3cc6e
Shovon Hasan <im.shovonhasan@gmail.com>
2017-07-26 09:35:56 -0400

    Remove unnecessary initial state

--- client/src/store.ts
+++ client/src/store.ts
@@ -2,56 +2,13 @@ import { createStore, compose } from 'redux';
 import { persistStore } from 'redux-persist';
 import { rootReducer } from './reducers';
 
-const emptyAuthForm = { text: '', error: null };
-
-const blankStore = {
-  currentUser: {
-    email: null,
-    token: null
-  },
-  notifications: {
-    fromServer: []
-  },
-  enthusiasm: {
-    level: 1,
-    languageName: 'TypeScript'
-  },
-  courses: {
-    list: []
-  },
-  forms: {
-    signup: {
-      email: emptyAuthForm,
-      username: emptyAuthForm,
-      password: emptyAuthForm,
-      passwordConf: emptyAuthForm,
-      loading: false
-    },
-    login: {
-      email: emptyAuthForm,
-      password: emptyAuthForm,
-      loading: false
-    },
-    addCourse: {
-      name: { text: '' }
-    }
-  },
-  users: {
-    list: []
-  }
-};
-
 // tslint:disable:no-any
 // tslint:disable:no-string-literal
 let devtools: any = window['devToolsExtension']
   ? window['devToolsExtension']
   : (f: any) => f;
 
-const store = createStore<RootState>(
-  rootReducer,
-  blankStore,
-  compose(devtools())
-);
+const store = createStore<RootState>(rootReducer, compose(devtools()));
 
 persistStore(store);
 
