49c0bb332475d73bb0dddf330af171f835bc2c65
Shovon Hasan <im.shovonhasan@gmail.com>
2017-07-31 15:33:32 -0400

    Add FormTypes type

--- client/src/actions/form.ts
+++ client/src/actions/form.ts
@@ -1,34 +1,39 @@
 import * as constants from '../constants';
 
-export interface FormUpdate<T> {
+export type FormTypes = Product | Course;
+
+export interface FormUpdate<T extends FormTypes> {
   type: constants.UPDATE_FIELD_TEXT;
   key: keyof T;
   value: string;
 }
 
-export interface FormSubmit<T> {
+export interface FormSubmit<T extends FormTypes> {
   type: constants.SUBMIT_FORM;
   payload: T;
 }
 
-export interface FormReset<T> {
+export interface FormReset<T extends FormTypes> {
   type: constants.RESET_TEXT_FIELD;
   key: keyof T;
 }
 
-export type FormAction<T extends Product | Course> =
+export type FormAction<T extends FormTypes> =
   | FormUpdate<T>
   | FormReset<T>
   | FormSubmit<T>;
 
-export function submitForm<T>(payload: T): FormSubmit<T> {
+export function submitForm<T extends FormTypes>(payload: T): FormSubmit<T> {
   return {
     type: constants.SUBMIT_FORM,
     payload
   };
 }
 
-export function changeFormText<T>(key: keyof T, value: string): FormUpdate<T> {
+export function changeFormText<T extends FormTypes>(
+  key: keyof T,
+  value: string
+): FormUpdate<T> {
   return {
     type: constants.UPDATE_FIELD_TEXT,
     key,
@@ -36,7 +41,7 @@ export function changeFormText<T>(key: keyof T, value: string): FormUpdate<T> {
   };
 }
 
-export function resetFormText<T>(key: keyof T): FormReset<T> {
+export function resetFormText<T extends FormTypes>(key: keyof T): FormReset<T> {
   return {
     type: constants.RESET_TEXT_FIELD,
     key
