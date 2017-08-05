5dd4ddaeb0f42f8e005ffd995a9d74df231f1c16
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-05 17:48:27 -0400

    Add support for generating slugs and handling identical slugs to Product model

--- server/models/Product.js
+++ server/models/Product.js
@@ -12,10 +12,12 @@ const productSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
+    validate: [ validator.isAscii, 'Illegal characters in product name' ],
     required: 'Please give the product a name.'
   },
   description: {
     type: String,
+    validate: [ validator.isAscii, 'Illegal characters in store description' ],
     trim: true
   },
   slug: String,
@@ -24,4 +26,35 @@ const productSchema = new mongoose.Schema({
     trim: true
   },
   imageSrc: String
+});
+
+productSchema.index({
+  name: 'text',
+  description: 'text'
+});
+
+productSchema.pre('save', async function(next) {
+  /* When updating an entry in the product table, this will only be called if 
+  the  name is modified. */
+  if (!this.isModified('name')) {
+    return next();
+  }
+
+  // Enforce 60 character limit
+  this.name = this.name.split('').slice(0, 39);
+
+  // Generate slug
+  this.slug = slug(this.name);
+
+  // Handle slugs with potentially identical names
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+
+  // this.constructor? Because you can't reference Store before it's created
+  const productsWithSlug = await this.constructor.find({ slug: slugRegEx });
+
+  if (productsWithSlug.length) {
+    this.slug = `${this.slug}-${productsWithSlug.length + 1}`;
+  }
+
+  next();
 });
