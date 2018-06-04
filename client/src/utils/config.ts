544bdc49570e05b075c6e0db5e2ee1dbacdc66c9
anveio <im.shovonhasan@gmail.com>
2018-06-04 09:17:31 -0400

    Fix typo.

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,4 +1,4 @@
-const FRONTEND_HOST_NAME = `stripe-fullstack-example.shovonhasan.com/`;
+const FRONTEND_HOST_NAME = `stripe-fullstack-example.shovonhasan.com`;
 const API_HOST_NAME = 'afternoon-falls-86728.herokuapp.com';
 
 export const configureApiRoot = () => {
