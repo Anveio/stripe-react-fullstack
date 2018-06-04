3a2073fffbe286db76bcfde9bb9b3f41bf4afac0
anveio <im.shovonhasan@gmail.com>
2018-06-04 09:15:16 -0400

    Move constants.

--- client/src/constants/config.ts
+++
@@ -1,2 +0,0 @@
-const FRONTEND_HOST_NAME = `stripe-fullstack-example.shovonhasan.com/`;
-const API_HOST_NAME = 'afternoon-falls-86728.herokuapp.com';
--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,3 +1,6 @@
+const FRONTEND_HOST_NAME = `stripe-fullstack-example.shovonhasan.com/`;
+const API_HOST_NAME = 'afternoon-falls-86728.herokuapp.com';
+
 export const configureApiRoot = () => {
   const hostname = window && window.location && window.location.hostname;
   switch (hostname) {
