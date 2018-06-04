7e4ec0c4d96c904ee822a208d72cf7023f4cc3c0
anveio <im.shovonhasan@gmail.com>
2018-06-04 09:11:24 -0400

    Fix configureApiRoot.

--- client/src/constants/config.ts
+++ client/src/constants/config.ts
@@ -1,1 +1,2 @@
+const FRONTEND_HOST_NAME = `stripe-fullstack-example.shovonhasan.com/`;
 const API_HOST_NAME = 'afternoon-falls-86728.herokuapp.com';
--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,7 +1,7 @@
 export const configureApiRoot = () => {
   const hostname = window && window.location && window.location.hostname;
   switch (hostname) {
-    case API_HOST_NAME:
+    case FRONTEND_HOST_NAME:
       return `https://${API_HOST_NAME}/api`;
     case 'localhost':
       return 'http://localhost:4000/api';
