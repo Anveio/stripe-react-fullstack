8faf90a099a648b0d47c2476e8532e4b409d6e92
anveio <im.shovonhasan@gmail.com>
2018-06-07 19:01:32 -0400

    Remove unnecessary constant.

--- client/src/constants/config.ts
+++ client/src/constants/config.ts
@@ -4,11 +4,6 @@
 export const STRIPE_PUBLISHABLE_TEST_KEY = 'pk_test_lSPPphIp2RSq3TYbez575NQW';
 
 /**
- * Replace with the URL of the site you deployed to Netlify. Make sure it doesn't have "https://" at the beginning.
- */
-export const PRODUCTION_FRONTEND_HOSTNAME = `stripe-fullstack-example.shovonhasan.com`;
-
-/**
  * Replace with the URL of the site you deployed to Heroku. Make sure it doesn't have "https://" at the beginning.
  */
 export const PRODUCTION_API_HOSTNAME = 'afternoon-falls-86728.herokuapp.com';
--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,19 +1,13 @@
 import { compose } from 'redux';
-import {
-  PRODUCTION_FRONTEND_HOSTNAME,
-  PRODUCTION_API_HOSTNAME,
-  API_VERSION
-} from '../constants/config';
+import { PRODUCTION_API_HOSTNAME, API_VERSION } from '../constants/config';
 
 export const configureApiRoot = () => {
   const hostname = window && window.location && window.location.hostname;
   switch (hostname) {
-    case PRODUCTION_FRONTEND_HOSTNAME:
-      return `https://${PRODUCTION_API_HOSTNAME}/api/v${API_VERSION}`;
     case 'localhost':
       return `http://localhost:4000/api/v${API_VERSION}`;
     default:
-      throw new Error('Invalid hostname');
+      return `https://${PRODUCTION_API_HOSTNAME}/api/v${API_VERSION}`;
   }
 };
 
