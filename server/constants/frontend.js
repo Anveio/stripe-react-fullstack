4d7677d9a5bc6303016d4ce7c145037c4183923c
anveio <im.shovonhasan@gmail.com>
2018-06-07 19:07:34 -0400

    Add netlify app URL to FRONTEND_PROD_URLS.

--- server/constants/frontend.js
+++ server/constants/frontend.js
@@ -2,8 +2,14 @@ const FRONTEND_DEV_URLS = ['http://localhost:3000'];
 
 /**
  * Replace this with the actual URL of your website after it's been deployed.
+ * This is an array in case you want to deploy the client to multiple URLS,
+ * or have a custom domain for your Netlify app and also want to support the
+ * original application URL.
  */
-const FRONTEND_PROD_URLS = ['https://stripe-fullstack-example.shovonhasan.com'];
+const FRONTEND_PROD_URLS = [
+  'https://stripe-fullstack-example.shovonhasan.com',
+  'https://distracted-kowalevski-960eec.netlify.com/'
+];
 
 module.exports =
   process.env.NODE_ENV === 'production'
