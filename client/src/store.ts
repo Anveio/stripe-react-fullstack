6299be7d832efb217ead8b2ee97a30c6532d4bbe
anveio <im.shovonhasan@gmail.com>
2018-06-17 15:41:44 -0400

    Get redux persist working.

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,10 +1,21 @@
 import { createStore } from 'redux';
-import { persistStore } from 'redux-persist';
+import { persistStore, persistReducer } from 'redux-persist';
 import { rootReducer } from './rootReducer';
 import { devtools } from 'utils/config';
+import storage from 'redux-persist/lib/storage';
 
-const store = createStore(rootReducer, devtools);
+const persistedReducer = persistReducer(
+  {
+    key: 'root',
+    storage
+  },
+  rootReducer
+);
 
-persistStore(store);
+const configureStore = () => {
+  const store = createStore(persistedReducer, devtools);
+  const persistor = persistStore(store);
+  return { store, persistor };
+};
 
-export default store;
+export default configureStore;
