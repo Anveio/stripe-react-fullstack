ddf73ec73a110adc9876b340d48b6638e9044373
AngelusProductons <angelusproductions@gmail.com>
2023-05-07 21:28:44 -0700

    use test key

--- client/src/constants/config.ts
+++ client/src/constants/config.ts
@@ -1,7 +1,8 @@
 /**
  * Replace this with your Stripe API publishable key at https://dashboard.stripe.com/account/apikeys
  */
-export const STRIPE_PUBLISHABLE_TEST_KEY = 'pk_test_lSPPphIp2RSq3TYbez575NQW';
+export const STRIPE_PUBLISHABLE_TEST_KEY = 'pk_test_' 
+    + '51N3WiQFYHcKM5vsbDAEumut55G5Zm7sZBGWQctLpMZU12o1NxnB7sx7oWPnUzN2jGodLFNHwomR5GJrFxiEjP5uM00LhzdOKk7';
 
 /**
  * Replace with the URL of the site you deployed to Heroku. Make sure it doesn't have "https://" at the beginning.
