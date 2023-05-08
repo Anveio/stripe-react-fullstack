c82dc9b1eabea65cf4588be41012b1bcf9301d2b
AngelusProductons <angelusproductions@gmail.com>
2023-05-07 23:40:21 -0700

    initial commit

--- client/src/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import authForms from './reducers/formAuth';
-import currentUser from './reducers/currentUser';
-import { RootState } from 'types';
-
-export const rootReducer = combineReducers<RootState>({
-  currentUser,
-  authForms
-});
--- client/src/utils/history.ts
+++
@@ -1,8 +0,0 @@
-import createBrowserHistory from 'history/createBrowserHistory';
-
-export const freshHistory = createBrowserHistory();
-
-/**
- * Curried version of history.pushState that leaves the first two arguments blank.
- */
-export const pushToAppHistory = (newState: string) => freshHistory.push('/');
