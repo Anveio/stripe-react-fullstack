5d62a22a829397307bc5a0e890d8985b9c54aa29
AngelusProductons <angelusproductions@gmail.com>
2023-05-15 00:04:56 -0700

    Change it to $1

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -46,7 +46,7 @@ class App extends React.PureComponent<Props & Handlers, never> {
                 <Checkout
                   name={'Example Item'}
                   description={'Example description'}
-                  amount={1}
+                  amount={100}
                 />
               )}
             />
