a263965ee550837278b2cc5a082a9de6037fcbb6
AngelusProductons <angelusproductions@gmail.com>
2023-05-14 22:57:28 -0700

    Upddate stuff

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -10,7 +10,7 @@ import { PRODUCTION_API_HOSTNAME, API_VERSION } from '../constants/config';
 export const configureApiRoot = () => {
   const hostname = window && window.location && window.location.hostname;
   switch (hostname) {
-    case 'localhost':
+    case 'localhost-test':
       return `http://localhost:4000/api/v${API_VERSION}`;
     default:
       return `https://${PRODUCTION_API_HOSTNAME}/api/v${API_VERSION}`;
