287e4db435a806f653c524c694d44097c18a9d25
AngelusProductons <angelusproductions@gmail.com>
2023-06-19 00:15:46 -0700

    Use live stripe key

--- server/constants/stripe.js
+++ server/constants/stripe.js
@@ -2,7 +2,7 @@ const Stripe = require('stripe');
 
 const STRIPE_SECRET_KEY =
   process.env.NODE_ENV === 'production'
-    ? process.env.STRIPE_SECRET_TEST // Add a live key if you're charging customers for real.
+    ? process.env.STRIPE_SECRET_LIVE // Add a live key if you're charging customers for real.
     : process.env.STRIPE_SECRET_TEST;
 
 module.exports = Stripe(STRIPE_SECRET_KEY);
